bdd43a2fb6c63a50afd5d72da36c716201c08039
Imran Zaman <imran.zaman@intel.com>
2013-09-19 13:54:40 +0300

    added enable-tests flag to run the tests using make check

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -3,10 +3,10 @@ export G_MESSAGES_DEBUG=all
 
 make distclean;
 autoreconf -fi;
-./configure --enable-dbus-type=session --enable-debug && \
+./configure --enable-dbus-type=session --enable-tests && \
 make -j4 && make check && make distclean && \
-./configure --enable-dbus-type=system --enable-debug && \
+./configure --enable-dbus-type=system --enable-tests && \
 make -j4 && make check && make distclean && \
-./configure --enable-dbus-type=p2p --enable-debug && \
+./configure --enable-dbus-type=p2p --enable-tests && \
 make -j4 && make check && make distclean;
 
